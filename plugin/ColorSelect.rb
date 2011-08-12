f1be920e3a2fdb7ec219ec37b44c850eb30f8d27
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-08-12 15:10:12 -0300

    Evoluindo interface para poder selecionar o formato que será inserido

--- plugin/ColorSelect.rb
+++ plugin/ColorSelect.rb
@@ -1,34 +1,104 @@
 require 'gtk2'
 
-def lowbase(value)
-  lowbase = value * 256 / 65536
+class Color
+  attr_accessor :color, :alpha
+
+  def initialize(color_selector)
+    @color = color_selector.current_color
+      @alpha = color_selector.current_alpha
+  end
+
+  def get_color(type)
+    case type
+    when :hex
+      "##{round_hex(@color.red)}#{round_hex(@color.green)}#{round_hex(@color.blue)}"
+    when :rgb
+      "rgb(#{rgb_value})"
+    when :rgba
+      "rgba(#{rgb_value}, #{"%.2f" % (lowbase(@alpha).to_f/255.to_f)})"
+    end
+  end
+
+  private
+  def rgb_value
+    "#{lowbase(@color.red)}, #{lowbase(@color.green)}, #{lowbase(@color.blue)}"
+  end
+
+  def lowbase(value)
+    value * 256 / 65536
+  end
+
+  def round_hex(value)
+    lowbase(value).to_s(16).ljust(2,'0')
+  end
+
 end
 
-def round_hex(value)
-  lowbase(value).to_s(16).ljust(2,'0')
-end
+window = Gtk::Window.new(Gtk::Window::TOPLEVEL)
+window.set_title  "VIM Color Selector"
+window.border_width = 10
+window.set_size_request(-1,-1)
 
-def color_to_hex(color)
-  "##{round_hex(color.red)}#{round_hex(color.green)}#{round_hex(color.blue)}"
-end
+window.signal_connect('delete_event') { Gtk.main_quit }
 
-def color_to_rgba(color, alfaObj)
-  "rgba(#{lowbase(color.red)}, #{lowbase(color.green)}, #{lowbase(color.blue)}, #{"%.2f" % (lowbase(alfaObj).to_f/255.to_f)})"
-end
+vbox = Gtk::VBox.new false, 0 
 
-colorSelection = Gtk::ColorSelectionDialog.new("VIM Color Selector")
-colorsel = colorSelection.colorsel
-colorsel.has_opacity_control = true
+color_selector = Gtk::ColorSelection.new
+color_selector.has_opacity_control = true
 
-response = colorSelection.run
+notebook = Gtk::Notebook.new
+notebook.show_tabs = false
 
-if response == Gtk::Dialog::RESPONSE_OK
-  colorObj = colorsel.current_color
-  alfaObj = colorsel.current_alpha
-  if ARGV.first == 'rgba'
-    print color_to_rgba(colorObj, alfaObj)
-  else
-    color = color_to_hex(colorObj)
-    print color.send(ARGV.last || 'downcase')
+notebook.append_page color_selector
+
+vbox.pack_start notebook, true, false, 0 
+
+radio = {}
+radio[:hex] = Gtk::RadioButton.new "Hexadecimal"
+radio[:rgb] = Gtk::RadioButton.new radio[:hex], "rgb"
+radio[:rgba] = Gtk::RadioButton.new radio[:rgb], "rgba"
+
+button_ok = Gtk::Button.new 'Ok'
+button_cancel = Gtk::Button.new 'Cancel'
+
+
+radios_box = Gtk::HBox.new false, 0 
+radios_box.pack_start radio[:hex], true, false, 0
+radios_box.pack_start radio[:rgb], true, false, 0
+radios_box.pack_start radio[:rgba], true, false, 0
+
+actions_box = Gtk::HBox.new false, 0
+actions_box.pack_start button_ok, true, false, 15
+actions_box.pack_start button_cancel, true, false, 15
+
+vbox.pack_start radios_box, true, false, 0
+vbox.pack_start actions_box, true, false, 0
+
+hbox = Gtk::HBox.new false, 0 
+hbox.pack_start vbox, false, false, 0
+
+if ARGV.first
+  if %w(hex rgb rgba).include?( ARGV.first.downcase || 'hex' )
+    radio[ARGV.first.downcase.to_sym].set_active true
   end
 end
+
+button_ok.signal_connect 'clicked' do
+
+  radio.select! { |key, radio_button| radio_button.active? } 
+
+  color_object = Color.new(color_selector)
+
+  print color = color_object.get_color(radio.keys.first).send(ARGV.last || 'downcase')
+
+  Gtk.main_quit
+
+end
+
+button_cancel.signal_connect 'clicked' do
+  Gtk.main_quit
+end
+
+window.add(hbox)
+window.show_all
+Gtk.main
