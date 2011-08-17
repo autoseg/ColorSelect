8844999f1582dd5c322ea1871546b56b04f782d6
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-08-17 02:41:53 -0300

    adicionando opção de selecionar na interface o case(maiuscula, minuscula) da cor

--- plugin/ColorSelect.rb
+++ plugin/ColorSelect.rb
@@ -51,27 +51,35 @@ notebook.show_tabs = false
 
 notebook.append_page color_selector
 
-vbox.pack_start notebook, true, false, 0 
+  vbox.pack_start notebook, true, false, 0 
 
-radio = {}
-radio[:hex] = Gtk::RadioButton.new "Hexadecimal"
-radio[:rgb] = Gtk::RadioButton.new radio[:hex], "rgb"
-radio[:rgba] = Gtk::RadioButton.new radio[:rgb], "rgba"
+string_case = {}
+string_case[:upcase] = Gtk::RadioButton.new "UPCASE"
+string_case[:downcase] = Gtk::RadioButton.new string_case[:upcase], "downcase"
+
+strings_case_box = Gtk::HBox.new false, 0 
+strings_case_box.pack_start string_case[:upcase], true, false, 0
+strings_case_box.pack_start string_case[:downcase], true, false, 0
+
+format = {}
+format[:hex] = Gtk::RadioButton.new "Hexadecimal"
+format[:rgb] = Gtk::RadioButton.new format[:hex], "rgb"
+format[:rgba] = Gtk::RadioButton.new format[:rgb], "rgba"
+
+formats_box = Gtk::HBox.new false, 0 
+formats_box.pack_start format[:hex], true, false, 0
+formats_box.pack_start format[:rgb], true, false, 0
+formats_box.pack_start format[:rgba], true, false, 0
 
 button_ok = Gtk::Button.new 'Ok'
 button_cancel = Gtk::Button.new 'Cancel'
-
-
-radios_box = Gtk::HBox.new false, 0 
-radios_box.pack_start radio[:hex], true, false, 0
-radios_box.pack_start radio[:rgb], true, false, 0
-radios_box.pack_start radio[:rgba], true, false, 0
 
 actions_box = Gtk::HBox.new false, 0
 actions_box.pack_start button_ok, true, false, 15
 actions_box.pack_start button_cancel, true, false, 15
 
-vbox.pack_start radios_box, true, false, 0
+vbox.pack_start strings_case_box, true, false, 0
+vbox.pack_start formats_box, true, false, 0
 vbox.pack_start actions_box, true, false, 0
 
 hbox = Gtk::HBox.new false, 0 
@@ -79,17 +87,25 @@ hbox.pack_start vbox, false, false, 0
 
 if ARGV.first
   if %w(hex rgb rgba).include?( ARGV.first.downcase || 'hex' )
-    radio[ARGV.first.downcase.to_sym].set_active true
+    format[ARGV.first.downcase.to_sym].set_active true
+  end
+end
+
+if ARGV[1]
+  if %(upcase downcase).include?( ARGV[1].downcase || 'downcase')
+    string_case[ARGV[1].downcase.to_sym].set_active true
   end
 end
 
 button_ok.signal_connect 'clicked' do
 
-  radio.select! { |key, radio_button| radio_button.active? } 
+  format.select! { |key, radio_button| radio_button.active? } 
+
+  string_case.select! { |key, radio_button| radio_button.active? }
 
   color_object = Color.new(color_selector)
 
-  print color = color_object.get_color(radio.keys.first).send(ARGV.last || 'downcase')
+  print color = color_object.get_color(format.keys.first).send(string_case.keys.first)
 
   Gtk.main_quit
 
